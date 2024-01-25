6c3673e777fee9c096a07fa8bac2b249caa3116e
MusagitovQA <musagitov.qa@mail.ru>
2024-01-26 00:21:18 +0400

    add allure

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,4 +1,5 @@
 import time
+import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -6,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from pages.form_page import Form_page
 
 
+@allure.description("Test form fill")
 def test_form():  # Настройки Chromedriver
     options = Options()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -21,5 +23,5 @@ def test_form():  # Настройки Chromedriver
     fp.input_form()  # Вызываем метод из  Form_page
 
     print("Finish Test")  # Вывод на экран конец теста
-    time.sleep(10)  # Ожидание 10сек
+    time.sleep(5)  # Ожидание 10сек
     driver.quit()  # Закрываем драйвер
